d86f137fa31671de75b3b1e0e37c7f9e1e90eda9
Luan Mollakuqe <luan.mollakuqe@infotiv.se>
2020-07-04 01:43:07 +0200

    added testnotebook.ipynb

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ portfolio_list = ["AMZN", # Amazon
 
 
 my_portfolio = Portfolio(portfolio_list)
-portfolio_correlation = my_portfolio.portfolio_correlation("5y")
-print(portfolio_correlation)
+my_portfolio_returns = my_portfolio.portfolioReturns("5y")
+print(my_portfolio_returns)
+# portfolio_correlation = my_portfolio.portfolio_correlation("5y")
+# print(portfolio_correlation)
 
