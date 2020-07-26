0653f642b46b888e1110c517383a22ce4eda6b44
Luan Mollakuqe <luan.mollakuqe@infotiv.se>
2020-07-27 00:14:09 +0200

    changed how correlation_table function works

--- financelib/analytics.py
+++ financelib/analytics.py
@@ -1,8 +1,12 @@
 
 import pandas as pd
 
-def correlation_table(portfolio: pd.DataFrame):
+def correlation_table(portfolio, period):
     '''
     Takes in a portfolio of stocks (Dataframe) and returns a correlation table.
+
+    :param portfolio: Portfolio represented by stocks (tickers)
+    :type portfolio: Portfolio
+    :return: correlation table of all stocks in the portfolio
     '''
-    return portfolio.corr()
+    return portfolio.returns(period).corr()
